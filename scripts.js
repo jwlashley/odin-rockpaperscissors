ba6dc43e343d68aeb490f5051580f5d18cba4ca1
jwlashley <jwlashley@atmo.space>
2023-09-01 22:54:38 -0500

    Added scoring system and a round counter that counts rounds until 5 games have been played and then announces the winner and both participants' scores.

--- scripts.js
+++ scripts.js
@@ -5,22 +5,52 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection){
-        return("Draw! You both chose: " + playerSelection)
+        return draw;
     }
     if (playerSelection === "rock" && computerSelection === "scissors"){
-        return("You win! rock beats scissors!")
+        playerScore++;
+        return playerWin;
     }
     if (playerSelection === "paper" && computerSelection === "rock"){
-        return("You Win! paper beats rock!")
+        playerScore++;
+        return playerWin;
     }
     if (playerSelection === "scissors" && computerSelection === "paper"){
-        return("You win! scissors beats paper!")
+        playerScore++
+        return playerWin;
     }
     else {
-        return ("You lose! " + computerSelection + " beats " + playerSelection + "!")
+        computerScore++;
+        return playerLose;
     }
 }
 
-const playerSelection = prompt("Begin the game by typing 'Rock', 'Paper', or 'Scissors'").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
+let playerWin = "You win!"
+let playerLose = "You Lose!"
+let draw = "It's a draw!"
+
+function game() {
+    for (let roundCount = 0; roundCount <= 5; roundCount++){
+        let playerSelection = prompt("Begin the game by typing 'Rock', 'Paper', or 'Scissors'",).toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Your score = " + playerScore);
+        console.log("Computer's Score = " + computerScore);
+    }
+    if (playerScore > computerScore){
+        console.log("You win!");
+    }
+    else if (playerScore === computerScore){
+        console.log("It's a draw! What a game!");
+    }
+    else{
+        console.log("You lose! Try again!");
+    }
+}
+
+game();
+
+
+
