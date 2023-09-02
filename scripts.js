20b07467e8213cec40ce405ddde9a4f5b517a72f
jwlashley <jwlashley@atmo.space>
2023-09-01 22:37:54 -0500

    Added string comparator in playRound function to check for win/loss. Also changed player input to a prompt function.

--- scripts.js
+++ scripts.js
@@ -3,4 +3,24 @@ function getComputerChoice(){
     return validChoices[Math.floor(Math.random() * validChoices.length)]
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        return("Draw! You both chose: " + playerSelection)
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors"){
+        return("You win! rock beats scissors!")
+    }
+    if (playerSelection === "paper" && computerSelection === "rock"){
+        return("You Win! paper beats rock!")
+    }
+    if (playerSelection === "scissors" && computerSelection === "paper"){
+        return("You win! scissors beats paper!")
+    }
+    else {
+        return ("You lose! " + computerSelection + " beats " + playerSelection + "!")
+    }
+}
+
+const playerSelection = prompt("Begin the game by typing 'Rock', 'Paper', or 'Scissors'").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
